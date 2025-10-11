a99fbcbbe045a63162d4ae513a4bfa447c27686f
Vashni <vashninadar123@gmail.com>
2025-10-11 12:58:35 +0530

    few files added

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use("*", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => {
   console.log(`Profile service running on port ${PORT}`);
